d386c35fbdede83ff9bd40ee66054424761c1bcb
Turkalp Burak KAYRANCIOGLU <bkayranci@gmail.com>
2017-02-09 20:57:46 +0300

    parantez duzeltildi

--- lamp-server.sh
+++ lamp-server.sh
@@ -25,7 +25,7 @@ OPTIONS="web-server-kur hakkimda cikis"
                 apt-get install php7.0 libapache2-mod-php7.0
                 apt-get install php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php7.0-imap php7.0-mcrypt php7.0-ps php7.0-pspell php7.0-recode php7.0-snmp php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-sqlite
                 clear
-                echo apache uzerine gelip space (bosluk) tusu ile onaylayin. Yes-No sorusuna No cevabini verin.
+                echo apache uzerine gelip space-bosluk tusu ile onaylayin. Yes-No sorusuna No cevabini verin.
                 apt-get install phpmyadmin
                 chmod -R 777 /var/www/
                 service apache2 restart
